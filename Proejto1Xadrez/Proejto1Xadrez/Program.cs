1be13139668237de18145d521db44297323da999
Gabriel <gabrielfdamaceno@gmail.com>
2019-12-14 10:04:14 -0300

    Criando a classe posição

--- Proejto1Xadrez/Proejto1Xadrez/Program.cs
+++ Proejto1Xadrez/Proejto1Xadrez/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-
+using tabuleiro;
 namespace Proejto1Xadrez
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Posicao P = new Posicao(3,4);
+            Console.WriteLine("Posição: "+ P);
         }
     }
 }
