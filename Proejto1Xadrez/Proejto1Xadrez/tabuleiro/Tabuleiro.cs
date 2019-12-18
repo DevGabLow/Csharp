5acf25626ed178024422b83feca1c2e9fa3f8880
Gabriel <gabrielfdamaceno@gmail.com>
2019-12-17 22:30:49 -0300

    Tratando err com exception personalizada

--- Proejto1Xadrez/Proejto1Xadrez/tabuleiro/Tabuleiro.cs
+++ Proejto1Xadrez/Proejto1Xadrez/tabuleiro/Tabuleiro.cs
@@ -10,17 +10,42 @@
         {
             this.linhas = linhas;
             this.colunas = colunas;
-            pecas = new Peca[linhas,colunas];
+            pecas = new Peca[linhas, colunas];
         }
 
         public Peca peca(int linha, int coluna)
         {
             return pecas[linha, coluna];
         }
-        public void ColocarPeca(Peca p, Posicao pos)
+        public Peca peca(Posicao pos)
         {
+            return pecas[pos.linha, pos.coluna];
+        }
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            if (existePeca(pos)) { throw new TabuleiroException("Já existe uma peça nessa posição!"); }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha < 0 || pos.coluna >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
