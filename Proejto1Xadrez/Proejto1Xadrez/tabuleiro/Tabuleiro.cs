ea50bce55ff213d0d36c8f59af409792d2a9884a
Gabriel <gabrielfdamaceno@gmail.com>
2019-12-17 22:13:57 -0300

    Colocando peças no tabuleiro

--- Proejto1Xadrez/Proejto1Xadrez/tabuleiro/Tabuleiro.cs
+++ Proejto1Xadrez/Proejto1Xadrez/tabuleiro/Tabuleiro.cs
@@ -17,5 +17,10 @@
         {
             return pecas[linha, coluna];
         }
+        public void ColocarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
+        }
     }
 }
